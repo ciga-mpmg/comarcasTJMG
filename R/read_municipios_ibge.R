c9666fe34fda0853f8f50b1c6e48633c00a47d06
Rubem Dornas <rapdornas@gmail.com>
2023-10-27 10:59:30 -0400

    Adding 2021 IBGE file directly on package

--- R/read_municipios_ibge.R
+++ R/read_municipios_ibge.R
@@ -1,32 +1,13 @@
-#' Funcao que baixa o arquivo Excel contendo a listagem oficial de municipios cadastrados no IBGE.
+#' Funcao que lê o arquivo Excel contendo a listagem oficial de municipios cadastrados no IBGE para o ano 2021. Baixado de: "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2021/DTB_2021.zip" em 27/10/2023.
+
 #'
 #' @return Data frame dos municípios do IBGE
 #' @export
 read_municipios_ibge <- function() {
 
-  url_ibge <- "https://geoftp.ibge.gov.br/organizacao_do_territorio/estrutura_territorial/divisao_territorial/2021/DTB_2021.zip"
-
-  # Cria uma pasta temporaria
-  td <- tempdir()
-
-  # Cria um arquivo .zip temporario
-  tf <- tempfile(tmpdir = td, fileext = ".zip")
-
-  # Realiza o download do arquivo do IBGE e salva no arquivo temporario
-  utils::download.file(url_ibge, tf)
-
-  # Lista os arquivos zipados dentro do arquivo temporario
-  file_names <- utils::unzip(tf, list = TRUE)
-
-  # Realiza a extracao dos arquivos temporarios dentro da pasta temporaria
-  utils::unzip(tf, exdir = td, overwrite = TRUE)
-
   # Le dados do Excel de municipios
-  munic_ibge <- readxl::read_xls(file.path(td, file_names$Name[4])) |>
+  munic_ibge <- readxl::read_xls("data/RELATORIO_DTB_BRASIL_MUNICIPIO_2021.xls") |>
     janitor::clean_names()
-
-  # Deleta arquivos e pasta
-  unlink(td)
 
   munic_ibge
 }
